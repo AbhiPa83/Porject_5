de77b4d8deb62acfe7cc6a4e84185f8d97d89bf2
Abhijeet Pathak <139596018+AbhiPa83@users.noreply.github.com>
2024-05-06 04:00:19 +0530

    Add files via upload

---
+++ Modules/Private_VPC/variables.tf
@@ -0,0 +1,8 @@
+variable "region" {}
+variable "private_vpc_name" {}
+variable "private_vpc_cidr" {}
+variable "privatevpc_public_subnet_az1_cidr" {}
+variable "privatevpc_public_subnet_az2_cidr" {}
+# variable "privatevpc_route_table_public_cidr" {}
+variable "privatevpc_private_subnet_az1_cidr" {}
+variable "privatevpc_private_subnet_az2_cidr" {}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {}
+variable "public_vpc_name" {}
+variable "public_vpc_cidr" {}
+variable "publicvpc_public_subnet_az1_cidr" {}
+variable "publicvpc_public_subnet_az2_cidr" {}
+variable "publicvpc_public_route_table_public_cidr" {}
+variable "publicvpc_private_subnet_az1_cidr" {}
+variable "publicvpc_private_subnet_az2_cidr" {}
+variable "private_vpc_name" {}
+variable "private_vpc_cidr" {}
+variable "privatevpc_public_subnet_az1_cidr" {}
+variable "privatevpc_public_subnet_az2_cidr" {}
+# variable "privatevpc_route_table_public_cidr" {}
+variable "privatevpc_private_subnet_az1_cidr" {}
+variable "privatevpc_private_subnet_az2_cidr" {}
