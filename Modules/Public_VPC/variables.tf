e3c6132d1d5c04444d91d417acdda3f84cd22392
Abhijeet Pathak <139596018+AbhiPa83@users.noreply.github.com>
2024-05-06 04:02:20 +0530

    Delete Modules directory

--- Modules/Public_VPC/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {}
-variable "public_vpc_name" {}
-variable "public_vpc_cidr" {}
-variable "publicvpc_public_subnet_az1_cidr" {}
-variable "publicvpc_public_subnet_az2_cidr" {}
-variable "publicvpc_public_route_table_public_cidr" {}
-variable "publicvpc_private_subnet_az1_cidr" {}
-variable "publicvpc_private_subnet_az2_cidr" {}
